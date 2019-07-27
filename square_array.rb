34964cea03c7ad06dc127b95461084153f56182e
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 20:51:15 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num|
+    new_array << num**2
+  end
+  new_array
 end
+
+## Build a method called square_array
+## That squares each element of array of numbers
+## Returns a new array of these squared numbers
+## Don't use any built-in array methods other than .each
